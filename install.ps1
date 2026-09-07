10a4db3715847a2c98f543706ffe50f87d4bc127
Hiraeth-Wave <hiraeth_wave@163.com>
2026-09-07 22:54:52 +0800

    feat: 增加系统版本校验

--- install.ps1
+++ install.ps1
@@ -5,8 +5,38 @@ $url = 'https://asset.starbridge.ink/regionInfo.json'
 $dest = [System.IO.Path]::GetFullPath((Join-Path $env:APPDATA '..\LocalLow\Innersloth\Among Us\regionInfo.json'))
 $exitCode = 0
 
+$osInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -ErrorAction SilentlyContinue
+if ($null -eq $osInfo) {
+    $osVersion = [System.Environment]::OSVersion.Version
+    $osInfo = [PSCustomObject]@{
+        CurrentMajorVersionNumber = [int]$osVersion.Major
+        CurrentMinorVersionNumber = [int]$osVersion.Minor
+        CurrentBuildNumber        = [int]$osVersion.Build
+    }
+}
+$osMajor = [int]$osInfo.CurrentMajorVersionNumber
+$osMinor = [int]$osInfo.CurrentMinorVersionNumber
+$osBuild = [int]$osInfo.CurrentBuildNumber
+
+if ($osMajor -eq 6 -and $osMinor -eq 1) {
+    Write-Error 'Starbridge 安装脚本不支持当前操作系统版本，请升级至 Windows 10 或更高版本后再试。'
+    Write-Host ''
+    Write-Error '您仍然可以尝试手动安装 Starbridge 服务器。参见 https://starbridge.ink/docs/guide#win。'
+    Write-Host ''
+    Write-Host '按下 Enter 键退出...'
+    Read-Host
+    exit 1
+}
+
 try {
-    Write-Host '欢迎使用 Starbridge 服务器配置文件安装器！当前脚本版本为 v1.1.0。'
+    Write-Host '欢迎使用 Starbridge 服务器配置文件安装器！当前脚本版本为 v1.1.1。'
+
+    if ($osMajor -eq 10 -and $osBuild -lt 22000) {
+        Write-Warning 'Microsoft 已于 2025 年 10 月停止对 Windows 10 的支持，Starbridge 安装脚本可能在此设备上无法正常工作。'
+        Write-Host ''
+        Write-Warning 'Starbridge 将不会处理在 Windows 10 系统上发送问题的报告，我们建议您升级至 Windows 11。'
+    }
+
     New-Item -ItemType Directory -Force -Path (Split-Path -Parent $dest) | Out-Null
 
     Write-Host '开始下载服务器配置文件...'
